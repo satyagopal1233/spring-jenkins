84ec63b79db2d6dfc480799d07a15aef1561a62a
sganta <satyagopal1233@gmail.com>
2021-06-04 22:02:18 +0530

    my first commit

---
+++ src/main/java/com/gk/springjenkins/SpringJenkinsApplication.java
@@ -0,0 +1,23 @@
+package com.gk.springjenkins;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	@PostConstruct
+	public void pconstruct() {
+		LOG.info("***********Application started");
+	}
+	public static void main(String[] args) {
+		LOG.info("***** Application executed");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
